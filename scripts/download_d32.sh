119834b81a58fc54df73c30627a09b70515658d6
Mikita Balesni <mbalesni@gmail.com>
2025-10-22 18:04:34 +0000

    fix downloads script

--- scripts/download_d32.sh
+++ scripts/download_d32.sh
@@ -1,7 +1,7 @@
 hf download karpathy/nanochat-d32
-cp ~/.cache/huggingface/hub/models--karpathy--nanochat-d32/snapshots/016dba034c9c0ca9033ad1bc721bceff54680600/token_bytes.pt ~/.cache/nanochat/tokenizer/
-cp -p /Users/tomek/.cache/huggingface/hub/models--karpathy--nanochat-d32/snapshots/016dba034c9c0ca9033ad1bc721bceff54680600/token_bytes.pt ~/.cache/nanochat/tokenizer/
 mkdir -p ~/.cache/nanochat/tokenizer/
 mkdir -p ~/.cache/nanochat/chatsft_checkpoints/d32
+cp ~/.cache/huggingface/hub/models--karpathy--nanochat-d32/snapshots/016dba034c9c0ca9033ad1bc721bceff54680600/token_bytes.pt ~/.cache/nanochat/tokenizer/
+cp -p ~/.cache/huggingface/hub/models--karpathy--nanochat-d32/snapshots/016dba034c9c0ca9033ad1bc721bceff54680600/token_bytes.pt ~/.cache/nanochat/tokenizer/
 cp -p ~/.cache/huggingface/hub/models--karpathy--nanochat-d32/snapshots/016dba034c9c0ca9033ad1bc721bceff54680600/meta_000650.json ~/.cache/nanochat/chatsft_checkpoints/d32/
 cp -p ~/.cache/huggingface/hub/models--karpathy--nanochat-d32/snapshots/016dba034c9c0ca9033ad1bc721bceff54680600/model_000650.pt ~/.cache/nanochat/chatsft_checkpoints/d32/
